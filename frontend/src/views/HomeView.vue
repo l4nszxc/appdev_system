ee2c90582d640fb54fb2404e0e0e7464ec8df59c
Hernandez, Lans Lorence N. <hernandezlanslorence@gmail.com>
2024-10-28 00:05:54 +0800

    UPDATED

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>LANS LORENCE</h1>
+    <h1>JEROME</h1>
     <router-link to="/login">Login</router-link>
     <router-link to="/register">Register</router-link>
   </div>

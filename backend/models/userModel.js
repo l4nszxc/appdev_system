f4753cef5cfcf1d8d3356c1b5d037340b0cf2898
Hernandez, Lans Lorence N. <hernandezlanslorence@gmail.com>
2024-11-10 13:44:04 +0800

    updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -7,13 +7,6 @@ const createUser  = (userData, callback) => {
                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
   db.query(sql, [student_id, username, email, password, firstname, middlename, lastname, gender, birthdate, program], callback);
 };
-
-const findUserByStudentId = (student_id, callback) => {
-  const sql = 'SELECT * FROM users WHERE student_id = ?';
-  db.query(sql, [student_id], callback);
-};
-
-
 // Function to find a user by email
 const findUserByEmail = (email, callback) => {
   const sql = `SELECT * FROM users WHERE email = ?`;
@@ -23,5 +16,4 @@ const findUserByEmail = (email, callback) => {
 module.exports = {
   createUser,
   findUserByEmail,
-  findUserByStudentId,
 };
